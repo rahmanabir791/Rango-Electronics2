5c9aba1cf826e785596142457774b827987c050a
rahmanabir791 <md.rrc.abir@gmail.com>
2023-08-14 20:38:03 +0600

    start work for backend and create and design login , register page

---
+++ resources/views/back/auth/login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Login Page</title>
+    <!-- Required meta tags -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css">
+   <link rel="stylesheet"  href="{{asset('/')}}assets/back-asset/login.css">
+</head>
+<body>
+<div class="shimmer-bg"></div>
+<div class="login-container">
+    <h2>Login</h2>
+    <form>
+        <div class="form-group">
+            <input type="email" class="form-control" id="exampleInputEmail" placeholder="Email address" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password" required>
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck">
+            <label class="form-check-label" for="exampleCheck">Remember me</label>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block mt-3">Login</button>
+    </form>
+</div>
+</body>
+</html>
---
+++ resources/views/back/auth/register.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Register Page</title>
+    <!-- Required meta tags -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css">
+    <link rel="stylesheet"  href="{{asset('/')}}assets/back-asset/register.css">
+</head>
+<body>
+<div class="shimmer-bg"></div>
+<div class="register-container">
+    <h2>Register</h2>
+    <form>
+        <div class="form-group">
+            <input type="text" class="form-control" id="exampleInputName" placeholder="Full Name" required>
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control" id="exampleInputEmail" placeholder="Email address" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" id="exampleInputPassword" placeholder="Password" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" id="exampleInputConfirmPassword" placeholder="Confirm Password" required>
+        </div>
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck">
+            <label class="form-check-label" for="exampleCheck">I agree to the terms and conditions</label>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block mt-3">Register</button>
+    </form>
+</div>
+</body>
+</html>
